56bf10e0c74310ac96241d018b1c01e3da751339
Tchamna <tchamna@gmail.com>
2025-12-17 11:12:46 -0500

    Run step1_audio_processing_v2 in pipeline

--- step0_main_pipeline.py
+++ step0_main_pipeline.py
@@ -4,10 +4,9 @@ from pathlib import Path
 import os
 
 # List of step scripts in order
-# You can override the step1 implementation via `STEP1_SCRIPT` env var.
-STEP1_SCRIPT = os.getenv("STEP1_SCRIPT", "step1_audio_processing_v2.py")
 steps = [
-    STEP1_SCRIPT,
+    "step1_audio_processing.py",
+    "step1_audio_processing_v2.py",
     "step2_video_production.py",
     "step3_combine_videos.py",
     "step4_udemy_normalization.py",
